ac12759526336205bda9f922edc19945a27e0a33
Pablo Buceta Santos <pablobucsan@gmail.com>
2025-10-24 13:23:51 +0200

    Repositories filled with needed functionality

--- app/src/main/java/com/example/calenderintegration/model/Event.kt
+++ app/src/main/java/com/example/calenderintegration/model/Event.kt
@@ -6,11 +6,11 @@ package com.example.calenderintegration.model
 
 data class Event( //Needs to be as basic as possible when it comes to the structure of an event, have your apiEvent conversion follow this
     val id: String,
-    val summary: String,
-    val description: String,
-    val start: String,
-    val end: String,
-    val location: String = "",
+    var summary: String,
+    var description: String,
+    var start: String,
+    var end: String,
+    var location: String = "",
     val calendarEmail: String
 )
 
