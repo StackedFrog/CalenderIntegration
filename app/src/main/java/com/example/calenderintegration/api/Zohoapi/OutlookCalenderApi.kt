7cda4d2651be988d05297c95b69172b3a21a42e0
LinnHamade <linn.hamade0284@stud.hkr.se>
2025-10-24 13:00:24 +0200

    hii

---
+++ app/src/main/java/com/example/calenderintegration/api/Zohoapi/OutlookCalenderApi.kt
@@ -0,0 +1,4 @@
+package com.example.calenderintegration.api.Zohoapi
+
+class OutlookCalenderApi {
+}
--- app/src/main/java/com/example/calenderintegration/api/outlookapi/OutlookCalenderApi.kt
+++
@@ -1,4 +0,0 @@
-package com.example.calenderintegration.api.outlookapi
-
-class OutlookCalenderApi {
-}
